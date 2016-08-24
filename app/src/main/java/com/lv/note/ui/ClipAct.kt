1a0cf110b9d1eafb4e8ab6b21a66747290252514
lvyong <lvyong@retail-tek.com>
2016-08-24 13:50:20 +0800

    优化

--- app/src/main/java/com/lv/note/ui/ClipAct.kt
+++ app/src/main/java/com/lv/note/ui/ClipAct.kt
@@ -5,12 +5,14 @@ import android.content.Intent
 import android.os.Environment
 import android.text.TextUtils
 import android.view.View
-import android.view.WindowManager
 import com.lv.note.R
 import com.lv.note.base.BaseActivity
 import com.lv.note.util.CommonUtils
+import com.lv.note.util.changeTopBgColor
 import com.lv.note.util.openNewAct
 import kotlinx.android.synthetic.main.act_clipimage.*
+import rx.Observable
+import rx.Subscriber
 import java.io.File
 
 /**
@@ -22,33 +24,42 @@ import java.io.File
 class ClipAct : BaseActivity() {
     private var path: String? = null
     override fun loadLayoutId(): Int {
+        changeTopBgColor()
         return R.layout.act_clipimage
     }
 
 
     override fun bindListener() {
-        id_action_clip.setOnClickListener{
-            showLodingView()
-            Thread(Runnable {
+        id_action_clip.setOnClickListener {
+            addSubscription(Observable.create(Observable.OnSubscribe<String> { subscriber ->
                 val bitmap = id_clipImageLayout.clip()
                 val path = Clip_CACHE + System.currentTimeMillis() + ".png"
                 CommonUtils.savePhotoToSDCard(bitmap, path)
-                val intent = Intent()
-                intent.putExtra(Clip_PATH, path)
-                setResult(Activity.RESULT_OK, intent)
-                finish()
-            }).start()
-        }
-    }
+                subscriber.onNext(path)
+                subscriber.onCompleted()
+            }).doOnSubscribe { showLodingView() }
+                    .subscribe(object : Subscriber<String>() {
+                        override fun onError(e: Throwable?) {
+                            toastError("裁剪图片失败")
+                        }
 
-    override fun onDestroy() {
-        hideLodingView()
-        super.onDestroy()
+                        override fun onNext(t: String?) {
+                            val intent = Intent()
+                            intent.putExtra(Clip_PATH, path)
+                            setResult(Activity.RESULT_OK, intent)
+                            finish()
+                        }
+
+                        override fun onCompleted() {
+                            hideLodingView()
+                        }
+
+                    }))
+        }
     }
 
 
     override fun initData() {
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         path = intent.getStringExtra(Clip_PATH)
         if (TextUtils.isEmpty(path) || !File(path!!).exists()) {
             toastError("图片加载失败")
@@ -69,9 +80,10 @@ class ClipAct : BaseActivity() {
         val Clip_PATH = "Clip_PATH"
         val Clip_CACHE = "${Environment.getExternalStorageDirectory()}/tanxi/cache/"
 
-        fun startClipAct(activity: Activity,view:View, path: String) {
-            activity.openNewAct(Intent(activity, ClipAct::class.java).putExtra(Clip_PATH, path),view, IMAGE_COMPLETE)
+        fun startClipAct(activity: Activity, view: View, path: String) {
+            activity.openNewAct(Intent(activity, ClipAct::class.java).putExtra(Clip_PATH, path), view, IMAGE_COMPLETE)
         }
+
         fun startClipAct(activity: Activity, path: String) {
             activity.startActivityForResult(Intent(activity, ClipAct::class.java).putExtra(Clip_PATH, path), IMAGE_COMPLETE)
         }
